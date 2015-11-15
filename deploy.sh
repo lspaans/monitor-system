884648fb6f6a4023fd68aa0f29a1b3e5b1ec0230
Léon Spaans <leons@gridpoint.nl>
2015-11-15 09:59:41 +0100

    Implements a first operational version

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 #
-# deploy.sh: Idempotent script voor de installatie van Polaris op Vagrant
+# deploy.sh: Idempotent script deployment of 'monitor-system' on Vagrant
 #
 
 set -e
 
+
 install_packages () {
     while [ $# -ne 0 ] ; do
-        if [ -e /usr/share/doc/"$1" ] ; then
-            printf 'Package already installed: %s\n' "$1" >&2
+        if [ -e /usr/share/doc/"$1" ]; then
+            printf 'Package already installed: "%s"\n' "$1" >&2
             shift
         else
             apt-get -y install "$@"
@@ -17,12 +18,41 @@ install_packages () {
     done
 }
 
+
+install_config () {
+    CONFIG=/usr/local/etc/"$1"
+    printf 'Installing configuration: "%s"\n' "$CONFIG" >&2
+    if [ -L "$CONFIG" ]; then
+        printf 'Configuration already installed: "%s"\n' "$1" >&2
+    else
+        ln -sf /vagrant/etc/"$1" "$CONFIG"
+    fi
+}
+
+
 install_script () {
     SCRIPT=/usr/local/bin/"$1"
-    printf "Installing %s...\n" "$SCRIPT" >&2
+    printf 'Installing script: "%s"\n' "$SCRIPT" >&2
     cat > "$SCRIPT"
     chmod 755 "$SCRIPT"
 }
+
+
+install_cronjob () {
+    SCRIPT=/etc/cron.d/"$1"
+    printf 'Installing cronjob: "%s"\n' "$SCRIPT" >&2
+    cat > "$SCRIPT"
+    chmod 644 "$SCRIPT"
+}
+
+
+make_dir () {
+    printf 'Creating directory: "%s"\n' "$1" >&2
+    if [ ! -e "$1" ]; then
+        mkdir -p "$1"
+    fi
+}
+
 
 apt-get update
 
@@ -31,6 +61,15 @@ install_script monitor-system <<'EOF'
 cd /vagrant
 python bin/monitor-system "$@"
 EOF
+
+make_dir /usr/local/etc/monitor-system.d
+
+install_config monitor-system.conf
+
+install_config monitor-system.d/monitor-passwd.conf
+install_config monitor-system.d/monitor-netstat.conf
+
+install_cronjob monitor-system < /vagrant/etc/cron-monitor-system.conf
 
 install_packages \
     build-essential
